b5097dac12b323996d5155c23aa7ec542cc780ae
Ott <49874532+ottx96@users.noreply.github.com>
2021-03-13 19:37:30 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Binding IP Address: $(hostname -i)"
-cd /workspace && /ungit/bin/ungit --ungitBindIp $(hostname -i)
+cd /workspace && /ungit/bin/ungit --ungitBindIp $(hostname -i) --no-b --root-path /workspace
